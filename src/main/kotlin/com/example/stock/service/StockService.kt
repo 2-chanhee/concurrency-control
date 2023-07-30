4f66535c73a7e08249da7d62f0c10852b3dc96d1
2-chanhee <54311184+2-chanhee@users.noreply.github.com>
2023-07-30 14:34:55 +0900

    feat: Synchronized (#2)

--- src/main/kotlin/com/example/stock/service/StockService.kt
+++ src/main/kotlin/com/example/stock/service/StockService.kt
@@ -7,9 +7,15 @@ import javax.transaction.Transactional
 
 @Service
 class StockService (val stockRepository: StockRepository) {
-    // 재고 감소
-    @Transactional
-    fun decrease(id: Long, quantity: Long) {
+    /**
+     *  재고 감소
+     *  Synchronized - 해당 메소드를 한 스레드에서만 실행할 수 있다.
+     *  Synchronized 문제점
+     *  - 서버가 한대일 때만 동시성 제어를 보장한다. 멀티서버에서는 보장 못함
+     */
+    // @Transactional
+    @Synchronized
+    fun  decrease(id: Long, quantity: Long) {
         val stock: Stock = stockRepository.findById(id).orElseThrow()
 
         stock.decrease(quantity);
